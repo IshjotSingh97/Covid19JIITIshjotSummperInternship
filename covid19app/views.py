01adfb5daca222aaffc4defe981b9d3b1f58d71e
IshjotSingh97 <ishjotsinghahluwalia@gmail.com>
2020-05-04 00:48:37 +0530

    initial

---
+++ covid19app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def urgent(request):
+    return render(request,'urgent.html')
