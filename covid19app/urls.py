a3662b3f369bbbbcb88e50b1955133dd9867470d
IshjotSingh97 <ishjotsinghahluwalia@gmail.com>
2020-05-05 14:42:05 +0530

    news

--- covid19app/urls.py
+++ covid19app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('urgent',views.urgent,name='urgent')
+    path('urgent',views.urgent,name='urgent'),
+    path('techstack',views.techstack,name='techstack'),
+    path('trackingtable',views.trackingtable,name='trackingtable'),
+    path('getnews',views.getnews,name='getnews')
 ]
--- covid19app/views.py
+++ covid19app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
+import requests,json
+
 
 # Create your views here.
 def index(request):
@@ -7,3 +9,34 @@ def index(request):
 
 def urgent(request):
     return render(request,'urgent.html')
+
+def techstack(request):
+    return render(request,'techstack.html')
+
+def trackingtable(request):
+    return render(request,'trackingtable.html')
+
+def getnews(request):
+    try:
+        q = "coronavirus"
+        urlpart1 = "https://newsapi.org/v2/everything?q="
+        urlpart2 = "&sortBy=publishedAt&pageSize=12&language=en&apiKey="
+        API_KEY = "09ea697cc4764ec0ab4987df2ad097fc"
+        main_url = urlpart1+str(q)+urlpart2+API_KEY
+        # fetching data in json format
+        page = requests.get(main_url).json()
+        # getting all articles in a string article
+        articles = page["articles"]
+        mydict = {
+            "error":False,
+            "result":True,
+            "keyword":q,
+            "articles":articles
+        }
+        return render(request,'news.html',context=mydict)
+    except:
+        mydict = {
+            "error":True,
+            "result":False
+        }
+        return render(request,'news.html',context=mydict)
