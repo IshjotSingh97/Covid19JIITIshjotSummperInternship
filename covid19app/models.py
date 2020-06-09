b9847518171246d15b5998aaab4d48066759464f
IshjotSingh97 <ishjotsinghahluwalia@gmail.com>
2020-06-09 14:21:08 +0530

    deploy

--- covid19app/models.py
+++ covid19app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Myth(models.Model):
 	title = models.CharField(max_length=200)
-	subject = models.TextField(max_length=200)
+	subject = models.TextField(max_length=1000)
 
 class Comment(models.Model):
 	title = models.CharField(max_length=200)
